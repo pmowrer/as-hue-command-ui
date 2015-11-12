6be69d07fa92cbc24b4d3cfecabc76411ac1a26a
Patrick Mowrer <pmowrer@gmail.com>
2015-11-12 00:15:54 -0500

    Show icon for each light

--- src/containers/app.js
+++ src/containers/app.js
@@ -5,6 +5,14 @@ import {
   toggleLight
 }                         from '../actions';
 
+const ICONS = {
+  LCT001: 'a19',
+  LCT002: 'br30',
+  LCT003: 'gu10',
+  LLC001: 'lc_iris',
+  LST001: 'lightstrip'
+};
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -25,21 +33,44 @@ class App extends Component {
 
   render() {
     const {lights} = this.props;
+    const styles = this.constructor.styles;
 
     return (
       <ul>
-        {lights.map((light, i) =>
-          <li key={i}>
-            <span>{light.name}</span>
-            <button onClick={this.toggle.bind(this, light)}>
-              {light.state.on ? 'ON' : 'OFF'}
-            </button>
-          </li>
-        )}
+        {lights
+          .map((light, i) =>
+            <li key={i}>
+              <div style={styles.lightIcon(light.modelid)}></div>
+              <span>{light.name}</span>
+              <button style={styles.toggleButton}
+                      onClick={this.toggle.bind(this, light)}>
+                {light.state.on ? 'ON' : 'OFF'}
+              </button>
+            </li>
+          )}
       </ul>
     );
   }
 }
+
+App.styles = {
+  toggleButton: {
+    marginLeft: '20px',
+    width: '40px',
+    height: '40px'
+  },
+
+  lightIcon(modelid) {
+    return {
+      backgroundImage: `url('img/${ICONS[modelid]}_outline.svg')`,
+      width: '25px',
+      height: '25px',
+      backgroundSize: 'contain',
+      backgroundRepeat: 'no-repeat',
+      display: 'inline-block'
+    };
+  }
+};
 
 function mapStateToProps(state) {
   const {lights} = state;
